2a1077fb8c77f2c916ea346c0b05ac4c2a3ea85f
origin <git@github.com:Azaharov14/helloworld.git>
2024-06-12 19:25:19 +0300

    5 commit

--- src/Main.java
+++ src/Main.java
@@ -5,28 +5,24 @@ public class Main {
         taskOne();
         int year = 2024;
         if (isLeapYear(year)) {
-            System.out.println("Это высокосный год");
+            System.out.println(year + " год - высокосный год ");
         } else {
-            System.out.println("Это не высокосный год");
+            System.out.println(year + " год - не высокосный год");
         }
         taskTwo();
         int platform = 0;
         int year2 = 2010;
         isPhonePlatform(platform, year2);
-
         taskThree();
         int deliveryDistance = 95;
         calculateDeliveryDays(deliveryDistance);
     }
-
     public static void taskOne() {
         System.out.println(" \n  Задача №1 ");
     }
-
     public static void taskTwo() {
         System.out.println(" \n Задача №2 ");
     }
-
     public static void taskThree() {
         System.out.println(" \n  Задача №3 ");
         int deliveryDistance = 95;
@@ -37,12 +33,10 @@ public class Main {
             System.out.println("Доставки нет");
         }
     }
-
     public static boolean isLeapYear(int year) {
         // возвращаю true или false в main для последующего анализа (проверил, работает)
         return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
     }
-
     public static void isPhonePlatform(int platform, int year2) {
         boolean yearPhone = year2 <= 2015;
         if (platform == 0 && yearPhone) {
@@ -55,13 +49,11 @@ public class Main {
             System.out.print("Установите версию приложения для Android по ссылке");
         }
     }
-
     public static int calculateDeliveryDays(int distance) {
-
         int day = 0;
         if (distance <= 20) {
             day = 1;
-        } else if (distance < 60) {
+        } else if (distance <= 60) {
             day = 2;
         } else if (distance <= 100) {
             day = 3;
